80557511ace60ce66ddc4c0de2c2ca6799177496
Ratul <89194947+ratul-cefalo@users.noreply.github.com>
2022-12-02 07:49:32 +0000

    second puzzle implemented

--- src/day-2/rps.py
+++ src/day-2/rps.py
@@ -1,4 +1,5 @@
 from functools import partial, cache
+from icecream import ic
 from rps_input import given_cases
 
 # print(given_cases[-5:])
@@ -7,29 +8,55 @@ from rps_input import given_cases
 def checker(instance: str, cases: set[str]) -> bool:
     return True if instance in cases else False
 
-WIN_CASES = {'A Y', 'B Z', 'C X'}
+
+WIN_CASES = {'A Y', 'A B', 'B Z', 'B C', 'C X', 'C A'}
 does_win = partial(checker, cases=WIN_CASES)
 
-DRAW_CASES = {'A X', 'B Y', 'C Z'}
+DRAW_CASES = {'A X', 'A A', 'B Y', 'B B', 'C Z', 'C C'}
 is_draw = partial(checker, cases=DRAW_CASES)
+
 
 @cache
 def scorer(instance: str) -> int:
     shape_score = dict(
-        # A=1, B=2, C=3,
+        A=1, B=2, C=3,
         X=1, Y=2, Z=3
     )
     score = shape_score[instance[-1]]
-
-    if does_win(instance): score+=6
-    elif is_draw(instance): score+=3
-
+    if does_win(instance):
+        score += 6
+    elif is_draw(instance):
+        score += 3
     return score
 
-def total_score(cases=['A Y', 'B X', 'C Z']):
+
+def total_score_puzzle1(cases=['A Y', 'B X', 'C Z']):
     scores = map(scorer, cases)
     return sum(scores)
 
+
+def total_score_puzzle2(cases=['A Y', 'B X', 'C Z']):
+    '''X lose
+        Y draw
+        Z win
+    '''
+    transform = {
+        "A X": "A C",
+        "A Y": "A A",
+        "A Z": "A B",
+        "B X": "B A",
+        "B Y": "B B",
+        "B Z": "B C",
+        "C X": "C B",
+        "C Y": "C C",
+        "C Z": "C A"}
+    cases = map(transform.get, cases)
+    scores = map(scorer, cases)
+    return sum(scores)
+
+
 if __name__ == "__main__":
-    assert total_score, 15
-    print(total_score(given_cases))
+    assert total_score_puzzle1(), 15
+    assert total_score_puzzle2(), 12
+    ic(total_score_puzzle1(given_cases))
+    ic(total_score_puzzle2(given_cases))
