49b4f44e6285ff45dd15174c96aa0026a7ed0e7c
Ratul <89194947+ratul-cefalo@users.noreply.github.com>
2022-12-02 06:16:44 +0000

    skeleton of new puzzle

---
+++ src/day-2/rps.py
@@ -0,0 +1,21 @@
+from rps_input import given_cases
+
+# print(given_cases[-5:])
+
+
+def checker(instance: str, cases: set) -> bool:
+    pass
+
+
+def does_win(instance: str) -> bool:
+    win_cases: set
+    pass
+
+
+def is_draw(instance: str) -> bool:
+    draw_cases: set
+    pass
+
+
+def scorer(instance: str) -> int:
+    pass
