70b6b011c9010102f72140c651a4bb8b2fb6abe7
Ratul <89194947+ratul-cefalo@users.noreply.github.com>
2022-12-02 07:01:12 +0000

    first puzzle done

--- src/day-2/rps.py
+++ src/day-2/rps.py
@@ -1,21 +1,35 @@
+from functools import partial, cache
 from rps_input import given_cases
 
 # print(given_cases[-5:])
 
 
-def checker(instance: str, cases: set) -> bool:
-    pass
+def checker(instance: str, cases: set[str]) -> bool:
+    return True if instance in cases else False
 
+WIN_CASES = {'A Y', 'B Z', 'C X'}
+does_win = partial(checker, cases=WIN_CASES)
 
-def does_win(instance: str) -> bool:
-    win_cases: set
-    pass
+DRAW_CASES = {'A X', 'B Y', 'C Z'}
+is_draw = partial(checker, cases=DRAW_CASES)
 
-
-def is_draw(instance: str) -> bool:
-    draw_cases: set
-    pass
-
-
+@cache
 def scorer(instance: str) -> int:
-    pass
+    shape_score = dict(
+        # A=1, B=2, C=3,
+        X=1, Y=2, Z=3
+    )
+    score = shape_score[instance[-1]]
+
+    if does_win(instance): score+=6
+    elif is_draw(instance): score+=3
+
+    return score
+
+def total_score(cases=['A Y', 'B X', 'C Z']):
+    scores = map(scorer, cases)
+    return sum(scores)
+
+if __name__ == "__main__":
+    assert total_score, 15
+    print(total_score(given_cases))
