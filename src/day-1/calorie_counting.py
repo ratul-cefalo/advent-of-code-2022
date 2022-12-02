8c12765674b84ccd1bd1251b196a2f9321a42f4d
Ratul Roy <ratulroy20@gmail.com>
2022-12-03 00:33:36 +0600

    Refactor/cleanup (#3)
    * better (??!) logic

--- src/day-1/calorie_counting.py
+++ src/day-1/calorie_counting.py
@@ -1,13 +1,11 @@
-'''
+"""
 Day 1
-'''
+"""
 from heapq import heappush, nlargest
 from calorie_counting_input import input_str
 from icecream import ic
 
-
 all_inputs = input_str.split("\n")
-
 
 max_calorie = -1
 calorie_sum_list = []
@@ -17,11 +15,12 @@ for calorie in all_inputs:
         total += int(calorie)
         continue
     max_calorie = max(total, max_calorie)
-    heappush(calorie_sum_list,total)
+    heappush(calorie_sum_list, total)
     total = 0
 
-# puzzle 1
-ic(max_calorie)
+if __name__ == "main":
+    # puzzle 1
+    assert max_calorie == 69795
 
-#puzzle 2
-ic(sum(nlargest(3,calorie_sum_list)))
+    # puzzle 2
+    assert sum(nlargest(3, calorie_sum_list)) == 208437
--- src/day-2/rps.py
+++ src/day-2/rps.py
@@ -6,19 +6,16 @@ def checker(instance: str, cases: set[str]) -> bool:
     return True if instance in cases else False
 
 
-WIN_CASES = {'A Y', 'A B', 'B Z', 'B C', 'C X', 'C A'}
+WIN_CASES = {"A Y", "B Z", "C X"}
 does_win = partial(checker, cases=WIN_CASES)
 
-DRAW_CASES = {'A X', 'A A', 'B Y', 'B B', 'C Z', 'C C'}
+DRAW_CASES = {"A X", "B Y", "C Z"}
 is_draw = partial(checker, cases=DRAW_CASES)
 
 
 @cache
 def scorer(instance: str) -> int:
-    shape_score = dict(
-        A=1, B=2, C=3,
-        X=1, Y=2, Z=3
-    )
+    shape_score = dict(X=1, Y=2, Z=3)
     score = shape_score[instance[-1]]
     if does_win(instance):
         score += 6
@@ -27,27 +24,28 @@ def scorer(instance: str) -> int:
     return score
 
 
-def total_score_puzzle1(cases=['A Y', 'B X', 'C Z']):
+def total_score_puzzle1(cases=["A Y", "B X", "C Z"]):
     scores = map(scorer, cases)
     return sum(scores)
 
 
-def total_score_puzzle2(cases=['A Y', 'B X', 'C Z']):
-    '''
+def total_score_puzzle2(cases=["A Y", "B X", "C Z"]):
+    """
     X lose
     Y draw
     Z win
-    '''
+    """
     transform = {
-        "A X": "A C",
-        "A Y": "A A",
-        "A Z": "A B",
-        "B X": "B A",
-        "B Y": "B B",
-        "B Z": "B C",
-        "C X": "C B",
-        "C Y": "C C",
-        "C Z": "C A"}
+        "A X": "A Z",
+        "A Y": "A X",
+        "A Z": "A Y",
+        "B X": "B X",
+        "B Y": "B Y",
+        "B Z": "B Z",
+        "C X": "C Y",
+        "C Y": "C Z",
+        "C Z": "C X",
+    }
     cases = map(transform.get, cases)
     scores = map(scorer, cases)
     return sum(scores)
